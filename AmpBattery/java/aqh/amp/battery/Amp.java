72d98101b6aed9e2732c2f4a2ef7a22a45026f55
Anonymous2716 <78570872+Anonymous2716@users.noreply.github.com>
2022-12-30 02:43:24 +0600

    Update Amp.java
    Indentation.

--- AmpBattery/java/aqh/amp/battery/Amp.java
+++ AmpBattery/java/aqh/amp/battery/Amp.java
@@ -13,22 +13,27 @@ import java.lang.Thread;
 
 public class Amp extends Activity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    Window w = getWindow();
-    w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-    setContentView(R.layout.lifecycle);
-    BatteryManager batteryManager = (BatteryManager) getApplicationContext().getSystemService(Context.BATTERY_SERVICE);
-    final  TextView message = (TextView)findViewById(R.id.message);
-    Handler hh = new Handler();
-    hh.postDelayed(new Runnable(){
-     public void run(){
-     message.setText(getString(R.string.current , batteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)));
-    hh.postDelayed(this, 800);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        setContentView(R.layout.lifecycle);
+
+        BatteryManager batteryManager = (BatteryManager) getApplicationContext().getSystemService(Context.BATTERY_SERVICE);
+
+        final  TextView message = (TextView)findViewById(R.id.message);
+
+        Handler hh = new Handler();
+        hh.postDelayed(new Runnable(){
+            public void run(){
+                message.setText(getString(R.string.current , batteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)));
+                hh.postDelayed(this, 800);
             }
-     }, 800);
-  }
+        }, 800);
+    }
 }
 
 
